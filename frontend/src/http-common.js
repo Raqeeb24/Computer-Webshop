e5d3a3c0daaf70efc4bcc21bc26b87cc4fde60ac
CyberVenom24 <roboraqeeb@gmail.com>
2022-11-17 20:58:32 +0100

    4.0

---
+++ frontend/src/http-common.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: "https://backend-computer-webshop.vercel.app//api/v1/",
+    headers: {
+        "Content-type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+    }
+});
