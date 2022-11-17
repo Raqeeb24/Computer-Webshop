561911729a0e01f90265cc97aae5f7c5ac7608cf
CyberVenom24 <roboraqeeb@gmail.com>
2022-11-17 20:58:32 +0100

    4.0

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # Computer-Webshop
-Made with ReactJs
-# DE
-Alles funktionert in diesem Ordner. Das einzige ist, dass sich das Passwort nicht mehr im .env File befindet.
-# EN
-Everything is okay. Just the password of the .env file is missing.
+ Made with ReactJs
---
+++ frontend/src/services/computer.js
@@ -0,0 +1,46 @@
+import http from "../http-common";
+import axios from "axios";
+
+const baseURL = "https://backend-computer-webshop.vercel.app/api/v1"
+
+class ComputerDataService {
+  getAll(page = 0) {
+    return axios.get(`${baseURL}/computers?page=${page}`);
+    //return http.get(`/computers?page=${page}`);
+  }
+
+  get(id) {
+    return axios.get(`${baseURL}/computers/id/${id}`);
+    //return http.get(`/computers/id/${id}`);
+  }
+
+  find(query, by = "name", page = 0) {
+    return axios.get(`${baseURL}/computers?${by}=${query}&page=${page}`);
+    //return http.get(`/computers?${by}=${query}&page=${page}`);
+  } 
+
+  createReview(data) {
+    return axios.get(`${baseURL}/review-new`, data);
+    //return http.post("/review-new", data);
+  }
+
+  updateReview(data) {
+    return axios.get(`${baseURL}/review-edit`, data);
+    //return http.put("/review-edit", data);
+  }
+
+  deleteReview(id, userId) {
+    return http.delete(`/review-delete?id=${id}`, {data:{user_id: userId}});
+  }
+
+  getCpu(id) {
+    return http.get(`/computers/cpu`);
+  }
+
+  createComputer(data){
+    return http.post("")
+  }
+
+}
+
+export default new ComputerDataService();
