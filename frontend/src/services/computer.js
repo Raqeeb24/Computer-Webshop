d480fe193e5bdc7eba4b328cfa381d451e581532
CyberVenom24 <roboraqeeb@gmail.com>
2023-08-14 20:00:13 +0200

    testing of http services succeeded

--- frontend/src/services/computer.js
+++ frontend/src/services/computer.js
@@ -1,32 +1,24 @@
 import http from "../http-common";
-import axios from "axios";
-
-const baseURL = "https://backend-computer-webshop.vercel.app/api/v1"
 
 class ComputerDataService {
   getAll(page = 0) {
-    //return axios.get(`${baseURL}/computers?page=${page}`);
     return http.get(`/computers?page=${page}`);
   }
 
   get(id) {
-    return axios.get(`${baseURL}/computers/id/${id}`);
-    //return http.get(`/computers/id/${id}`);
+    return http.get(`/computers/id/${id}`);
   }
 
   find(query, by = "name", page = 0) {
-    //return axios.get(`${baseURL}/computers?${by}=${query}&page=${page}`);
     return http.get(`/computers?${by}=${query}&page=${page}`);
   } 
 
   createReview(data) {
-    return axios.post(`${baseURL}/computers/review-new`, data);
-    //return http.post("/review-new", data);
+    return http.post("/review-new", data);
   }
 
   updateReview(data) {
-    return axios.put(`${baseURL}/computers/review-edit`, data);
-    //return http.put("/review-edit", data);
+    return http.put("/review-edit", data);
   }
 
   deleteReview(id, userId) {
@@ -38,7 +30,7 @@ class ComputerDataService {
   }
 
   createComputer(data){
-    return axios.post(`${baseURL}/computers/computer`, data);
+    return http.post(`/computers/computer`, data);
   }
 
 }
