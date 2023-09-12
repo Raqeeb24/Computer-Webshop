c3635dc4dde8f3eb277f1b3c0685476d36621bbc
Raqeeb24 <r.mohamedazhar.inf20@stud.bbbaden.ch>
2023-09-12 14:05:32 +0200

    Update http-common.js

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -2,5 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
     //baseURL: "http://localhost:5000/api/v1",
-    baseURL: "https://backend-computer-webshop.vercel.app/api/v1/"
+    //baseURL: "https://backend-computer-webshop.vercel.app/api/v1/"
+    baseURL: "https://backend-computer-webshop-ri3m.vercel.app/api/v1"
 });
