320c500560d43d3a2debf5605decfe793c368533
Raqeeb24 <r.mohamedazhar.inf20@stud.bbbaden.ch>
2023-09-14 17:16:36 +0200

    added loading skeleton

--- frontend/src/components/computers-list.js
+++ frontend/src/components/computers-list.js
@@ -3,15 +3,21 @@ import ComputerDataService from "../services/computer";
 import { Link } from "react-router-dom";
 import "./computers-list.css";
 
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 const ComputersList = props => {
+  const [loading, setLoading] = useState(true);
   const [computers, setComputers] = useState([]);
   const [searchName, setSearchName] = useState("");
   const [searchCpu, setSearchCpu] = useState("");
   const [cpus, setCpu] = useState(["Search by CPU"]);
 
   useEffect(() => {
-    retrieveComputers();
+    setLoading(true);
     retrieveCpu();
+    retrieveComputers();
+    setLoading(false);
   }, []);
 
   const onChangeSearchName = e => {
@@ -91,78 +97,95 @@ const ComputersList = props => {
       })
   };
 
-    return (
-      <div>
-        <div className="row pb-1">
-          <div className="input-group col-lg">
-            <input
-              type="text"
-              className="form-control input"
-              placeholder="Search by name"
-              value={searchName}
-              onChange={onChangeSearchName}
-            />
-            <div className="input-group-append">
-              <button
-                className="btn btn-outline-secondary search-button"
-                type="button"
-                onClick={findByName}
-              >
-                Search
-              </button>
-            </div>
-          </div>
-          <div className="input-group col-lg">
-            <select className="form-control input" onChange={onChangeSearchCpu}>
-              {cpus.map(cpu => {
-                return (
-                  <option value={cpu}> {cpu.substr(0, 20)} </option>
-                )
-              })}
-            </select>
-            <div className="input-group-append">
-              <button
-                className="btn btn-outline-secondary search-button"
-                type="button"
-                onClick={findByCpu}
-              >
-                Search
-              </button>
-            </div>
-
+  return (
+    <div>
+      <div className="row pb-1">
+        <div className="input-group col-lg">
+          <input
+            type="text"
+            className="form-control input"
+            placeholder="Search by name"
+            value={searchName}
+            onChange={onChangeSearchName}
+          />
+          <div className="input-group-append">
+            <button
+              className="btn btn-outline-secondary search-button"
+              type="button"
+              onClick={findByName}
+            >
+              Search
+            </button>
           </div>
         </div>
-        <div className="row">
-          {computers.map((computer) => {
-            const components = `${computer.cpu} ${computer.mainboard}, ${computer.ram}`;
-            return (
-              <div className="col-lg-4 pb-1">
-                <Link to={"/computers/" + computer._id} className="card custom-card">
-                  <div className="card-body">
-                    <h5 className="card-title">{computer.name}</h5>
-                    <p className="card-text">
-                      <strong>CPU: </strong>{computer.cpu}<br />
-                      <strong>RAM: </strong>{computer.ram}<br />
-                      <strong>GPU: </strong>{computer.gpu}<br />
-                      <strong>Price: </strong>{computer.price}
-                    </p>
-                    <div className="row justify-content-center">
-                      <button onClick={(event) => {
-                          event.preventDefault();
-                          handleAddToCart(computer);
-                        }} 
-                        className="btn btn-primary col-lg-5 mx-1 mb-1 add-to-cart-button">
-                        Add to cart
-                      </button>
-                    </div>
-                  </div>
-                </Link>
-              </div>
-            );
-          })}
+        <div className="input-group col-lg">
+          <select className="form-control input" onChange={onChangeSearchCpu}>
+            {cpus.map(cpu => {
+              return (
+                <option value={cpu}> {cpu.substr(0, 20)} </option>
+              )
+            })}
+          </select>
+          <div className="input-group-append">
+            <button
+              className="btn btn-outline-secondary search-button"
+              type="button"
+              onClick={findByCpu}
+            >
+              Search
+            </button>
+          </div>
+
         </div>
       </div>
-    );
-  };
+      <div className="row">
+        {loading ? (
+          Array.from({ length: 9 }).map((index) => (
+            <div className="col-lg-4 pb-1" key={index}>
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">
+                    <Skeleton width={200} />
+                  </h5>
+                  <p className="card-text">
+                    <Skeleton count={3} />
+                    <Skeleton width={100} />
+                  </p>
+                  <div className="row justify-content-center">
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))
+        ) : (
+          computers.map((computer) => (
+            <div className="col-lg-4 pb-1">
+              <Link to={"/computers/" + computer._id} className="card custom-card">
+                <div className="card-body">
+                  <h5 className="card-title">{computer.name || <Skeleton />}</h5>
+                  <p className="card-text">
+                    <strong>CPU: </strong>{computer.cpu}<br />
+                    <strong>RAM: </strong>{computer.ram}<br />
+                    <strong>GPU: </strong>{computer.gpu}<br />
+                    <strong>Price: </strong>{computer.price}
+                  </p>
+                  <div className="row justify-content-center">
+                    <button onClick={(event) => {
+                      event.preventDefault();
+                      handleAddToCart(computer);
+                    }}
+                      className="btn btn-primary col-lg-5 mx-1 mb-1 add-to-cart-button">
+                      Add to cart
+                    </button>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
 
-  export default ComputersList;
+export default ComputersList;
